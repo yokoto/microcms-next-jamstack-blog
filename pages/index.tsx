1d4d9187f97fa26fdc6d27431f4ab352a86d2d89
yokoto <youkotoushi@gmail.com>
2022-06-04 22:24:12 +0900

    add mui

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,13 @@ import type { Blog } from '../types/blog';
 import Link from "next/link";
 import { client } from "../libs/client";
 
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+// import Link from '../src/Link';
+// import ProTip from '../src/ProTip';
+import Copyright from '../Copyright';
+
 type Props = {
   blogs: Blog[]
 }
@@ -12,8 +19,20 @@ const Home: NextPage<Props> = (props: Props) => {
   const { blogs } = props;
 
   return (
-    <div>
-      <ul>
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          yokoto's tech blog
+        </Typography>
+        <ul>
         {blogs.map((blog: Blog) => (
           <li key={blog.id}>
             <Link href={`/blog/${blog.id}`}>
@@ -21,15 +40,13 @@ const Home: NextPage<Props> = (props: Props) => {
             </Link>
           </li>
         ))}
-      </ul>
-    </div>
+        </ul>
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
 
-// データをテンプレートに受け渡す部分の処理を記述します
-// これは、ビルド時にサーバー側で呼ばれる関数です
-// この部分の処理は最終的にバンドルJSに含まれません
-// ビルド時にデータを取得し、静的なHTMLを出力するために必要です
 export const getStaticProps: GetStaticProps<Props> = async () => {
   const data = await client.get({ endpoint: "blog" });
 
